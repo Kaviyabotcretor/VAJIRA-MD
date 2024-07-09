742654d303232a4a85da3bfa41a9b47ce11515b4
Kaviyabotcretor <ravindukavintha5@gmail.com>
2024-07-09 17:43:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '940717267602' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=Kc0HSTrL#GvausnjP-Gy973ae-Qk4AjYyj4D0qgh9zifG-8AtUmE' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=3VMGXKBK#7Pi809TCJwqhE_1EYdkTnfyaTOfbpFYm8Oy2dm5BtQg' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
